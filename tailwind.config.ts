110750073270f097157d101a98aac20a0be6e169
yyobibenb <yyobibenb@gmail.com>
2025-09-01 13:53:12 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -138,5 +138,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
